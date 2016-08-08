4c363972d52d26ae2edd8ddb1cc6d21c2b1993f0
Katoch <rkatoch@paypalcorp.com>
2016-08-08 11:11:31 +0530

    Added sentence length function

--- src/Book.java
+++ src/Book.java
@@ -1,43 +1,106 @@
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
+import com.sun.media.sound.InvalidFormatException;
+
+import opennlp.tools.sentdetect.SentenceDetectorME;
+import opennlp.tools.sentdetect.SentenceModel;
+
 class Book {
-	
-	private List<String> book;
-	
+
+	private String book;
+	private String[] sentences;
+	public static String TRAINING_FILE = "C:/Users/abiak/Downloads/en-sent.bin";
+	private int sentenceCount;
+	private int wordCount;
+
+	/*	Constructors
+	 * 	default constructor for initialization
+	 *	parameterized constructor for file reading
+	 */
 	public Book() {
 		book = null;
 	}
 	public Book(String filename) throws IOException {
 		book = readFile(filename);
+		sentences = SentenceDetect(book);
+		sentenceCount=sentences.length;
 	}
-	
+
+	/*
+	 * Function: getBookList
+	 * Input : None
+	 * Output : List of Strings containing all lines of the file.
+	 * 
+	 */
+	private String getBookList() {
+		return this.book;
+	}
+
+
+	/*
+	 * Function: getSentenceList
+	 * Input : None
+	 * Output : List of Strings containing all lines of the file.
+	 * 
+	 */
+	private String[] getSentenceList() {
+		return this.sentences;
+	}
+
+	/*
+	 * Function: getSentenceCount
+	 * Input : None
+	 * Output : Sentence Count of this book
+	 * 
+	 */
+	private int getSentenceCount() {
+		return this.sentenceCount;
+	}
+
+	/*
+	 * Function: getWordCount
+	 * Input : None
+	 * Output : Word Count of this book
+	 * 
+	 */
+	private int getWordCount() {
+		return this.wordCount;
+	}
+
 	/*
 	 * Function: readFile
 	 * Input : Path of the file (String)
 	 * Output : List of Strings containing all lines of the file.
 	 * 
 	 */
-	private static List<String> readFile(String filePath) throws IOException{
-		List<String> book = new ArrayList<>();
-		book = (List<String>) Files.readAllLines(Paths.get(filePath));
-		return book;
+	public static String readFile(String path) throws IOException{
+		File file = new File(path);
+		FileInputStream fis = new FileInputStream(file);
+		byte[] data = new byte[(int) file.length()];
+		fis.read(data);
+		fis.close();
+		String str = new String(data, "UTF-8");
+		return str;
 	}
-	
+
 	/*
 	 * Function: getTopNWords
-	 * Input : Map with word frequence (Map), Number of words to give as output (Integer)
+	 * Input : Map with word frequency (Map), Number of words to give as output (Integer)
 	 * Output : List of Strings top N words.
 	 * 
 	 */
 	private List<String> getTopNWords(Map<Integer,List<String>> wordFrequency, int N) {
 		return null;
 	}
-	
+
 	/*
 	 * Function: getWordFrequency
 	 * Input : All lines of the file (List<String>)
@@ -47,14 +110,31 @@ class Book {
 	private Map<String,Integer> getWordFrequency(List<String> book) {
 		return null;
 	}
-	
+
 	/*
-	 * Function: readFile
+	 * Function: getAverageSentenceLength
 	 * Input : Path of the file (String)
 	 * Output : List of Strings containing all lines of the file.
 	 * 
 	 */
-	private Integer getAverageSentenceLength(List<String> book) {
-		return 0;
+	private int getAverageSentenceLength(String book) {
+		return wordCount/sentenceCount;
 	}
+	
+	private String[] SentenceDetect(String data) throws InvalidFormatException,IOException {
+		
+		// always start with a model, a model is learned from training data
+		InputStream is = new FileInputStream(TRAINING_FILE);
+		SentenceModel model = new SentenceModel(is);
+		SentenceDetectorME sdetector = new SentenceDetectorME(model);
+
+		String sentences[] = sdetector.sentDetect(data);
+
+		System.out.println(sentences.length);
+		
+		is.close();
+		return sentences;
+	}
+
+	 
 }
