cdd9f4fb3fb748bf860d3e3e5d30ae2cb07ad471
A K <abiak@paypalcorp.com>
2016-08-08 11:38:04 +0530

    Added word frequency function and moved few files to utils

--- src/Book.java
+++ src/Book.java
@@ -1,10 +1,12 @@
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -12,14 +14,17 @@ import com.sun.media.sound.InvalidFormatException;
 
 import opennlp.tools.sentdetect.SentenceDetectorME;
 import opennlp.tools.sentdetect.SentenceModel;
+import opennlp.tools.tokenize.Tokenizer;
+import opennlp.tools.tokenize.TokenizerME;
+import opennlp.tools.tokenize.TokenizerModel;
 
 class Book {
 
 	private String book;
 	private String[] sentences;
-	public static String TRAINING_FILE = "C:/Users/abiak/Downloads/en-sent.bin";
 	private int sentenceCount;
 	private int wordCount;
+	Map<String,Integer> wordFrequency=new HashMap<String,Integer>();
 
 	/*	Constructors
 	 * 	default constructor for initialization
@@ -28,22 +33,13 @@ class Book {
 	public Book() {
 		book = null;
 	}
+	
 	public Book(String filename) throws IOException {
 		book = readFile(filename);
 		sentences = SentenceDetect(book);
 		sentenceCount=sentences.length;
+		wordFrequency=getWordFrequency(sentences); 	
 	}
-
-	/*
-	 * Function: getBookList
-	 * Input : None
-	 * Output : List of Strings containing all lines of the file.
-	 * 
-	 */
-	private String getBookList() {
-		return this.book;
-	}
-
 
 	/*
 	 * Function: getSentenceList
@@ -107,8 +103,27 @@ class Book {
 	 * Output : Mapping of word to their frequency
 	 * 
 	 */
-	private Map<String,Integer> getWordFrequency(List<String> book) {
-		return null;
+	private Map<String,Integer> getWordFrequency(String[] sentences) throws InvalidFormatException, IOException {
+		InputStream is = new FileInputStream("utils/en-token.bin");
+		 
+		TokenizerModel model = new TokenizerModel(is);
+	 
+		Tokenizer tokenizer = new TokenizerME(model);
+		
+		Map<String,Integer> freqs=new HashMap<String,Integer>();
+		String[] tokens;
+		
+		for(String s:sentences){
+		  tokens = tokenizer.tokenize(s);
+			 for (String token : tokens) {
+				    if (freqs.containsKey(token)) {
+				      freqs.put(token, freqs.get(token) + 1);
+				    } else {
+				      freqs.put(token, 1);
+				    }
+			 }	
+		}
+		return freqs;
 	}
 
 	/*
@@ -124,13 +139,13 @@ class Book {
 	private String[] SentenceDetect(String data) throws InvalidFormatException,IOException {
 		
 		// always start with a model, a model is learned from training data
-		InputStream is = new FileInputStream(TRAINING_FILE);
+		InputStream is = new FileInputStream("utils/en-sent.bin");
 		SentenceModel model = new SentenceModel(is);
 		SentenceDetectorME sdetector = new SentenceDetectorME(model);
 
 		String sentences[] = sdetector.sentDetect(data);
 
-		System.out.println(sentences.length);
+		//System.out.println(sentences.length);
 		
 		is.close();
 		return sentences;
